73075f4bf8cd6ce354f35fa840d7951c08a4e3fe
SuperAlokB <40793481+SuperAlokB@users.noreply.github.com>
2024-07-01 18:07:15 +0530

    done basic level 1

--- SplitTeamBill/app/src/main/java/com/example/splitteambill/ui/dashboard/DashboardViewModel.kt
+++ SplitTeamBill/app/src/main/java/com/example/splitteambill/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This will show all split bill information "
     }
     val text: LiveData<String> = _text
 }
--- SplitTeamBill/app/src/main/java/com/example/splitteambill/ui/home/HomeViewModel.kt
+++ SplitTeamBill/app/src/main/java/com/example/splitteambill/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Add Group Members"
     }
     val text: LiveData<String> = _text
 }
